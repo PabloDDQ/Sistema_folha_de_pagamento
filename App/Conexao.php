69f826c968b8c4272ff6ebccc8623c45acff250a
Pablo Duarte de Quadros <pabloduartequadros@gmail.com>
2026-08-28 08:32:52 -0300

    Criado a classe conexão e movido a Model para dentro de app

---
+++ App/Conexao.php
@@ -0,0 +1,27 @@
+<?php
+
+class Conexao
+{
+    private static $instance;
+    public static function getConn()
+    {
+        if (!isset(self::$instance)) {
+            $env = parse_ini_file(__DIR__ . '/../dados_bd.env');
+            $host = $env['DB_HOST'];
+            $port = $env['DB_PORT'];
+            $dbname = $env['DB_NAME'];
+            $user = $env['DB_USER'];
+            $password = $env['DB_PASS'];
+
+            $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+
+            self::$instance = new PDO($dsn, $user, $password);
+
+            self::$instance->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+        }
+        return self::$instance;
+    }
+}
